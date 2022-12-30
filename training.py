9e2cee452c54255ef69e4fb70044b8ce8bafeb77
BEHRAD <behrad.tajali@gmail.com>
2022-12-30 15:08:06 +0100

    First commit!

--- training.py
+++ training.py
@@ -7,9 +7,31 @@ def trainer(elm_type: str, dataset: str, trigger: str, hdlyr_size: int):
                 'drop-elm': drop_elm.DropClassifier}
 
     if elm_type.lower() == 'poelm':
-        elm = elm.ELMClassifier(hidden_layer_size=hdlyr_size, activation='sigm')
-        elm.fit(x_train, y_train)
+        poelm = elm.ELMClassifier(hidden_layer_size=hdlyr_size, activation='sigm')
+        poelm.fit(x_train, y_train)
         out = elm.predict(X)
+    elif elm_type.lower() == 'elm-pca':
+        pct = pca_transformed.PCTClassifier(hidden_layer_size=hdlyr_size, retained=None,
+                               activation='sigm')  # retained can be (0, 1) percent variation or an integer number of PCA modes to retain
+        pct.fit(X, y)
+        res = pct.predict(X)
+
+
+    elif elm_type.lower() == 'pca-elm':
+        pci = el.PCIClassifier(retained=None,
+                               activation='sigm')  # retained can be (0, 1) percent variation or an integer number of PCA modes to retain
+        pci.fit(X, y)
+        res = pci.predict(X)
+
+    elif elm_type.lower() == 'pruned-elm':
+        prune = el.PrunedClassifier(hidden_layer_size=hdlyr_size, activation='sigm')
+        prune.fit(X, y)
+        res = prune.predict(X)
+    elif elm_type.lower() == 'drop-elm':
+        drop = el.DropClassifier(hidden_layer_size=hdlyr_size, activation='sigm', dropconnect_pr=0.5, dropout_pr=0.5,
+                                 dropconnect_bias_pctl=0.9, dropout_bias_pctl=0.9)
+        drop.fit(X, y)
+        res = drop.predict(X)
 
 
 
