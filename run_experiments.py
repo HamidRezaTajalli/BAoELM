3ae21ce38af40ff26c1a72864cbccb361b6be3e0
HamidRezaTajalli <behrad.tajali@gmail.com>
2024-05-19 20:04:40 +0200

    update run experiments file

--- run_experiments.py
+++ run_experiments.py
@@ -25,31 +25,34 @@ target_label = 0
 
 
 for dataset in dataset_list:
-    for elm_type in elm_type_list:
-        for hdlyr_size in hdlyr_size_list:
-            for epsilon in epsilon_list:
-                for trigger_size in trigger_size_list:
-                    for exp_num in range(n_of_experiments):
-                        # Define the job script filename
-                        job_script_filename = f"job_{dataset}_{elm_type}_{hdlyr_size}_{epsilon}_{trigger_size[0]}x{trigger_size[1]}_{exp_num}.sh"
-                        job_script_path = Path(saving_path) / job_script_filename
+    for elm_type in elm_type_list:       
+        for epsilon in epsilon_list:
+            for trigger_size in trigger_size_list:
+                for exp_num in range(n_of_experiments):
+                    # Define the job script filename
+                    job_script_filename = f"job_{dataset}_{elm_type}_{epsilon}_{trigger_size[0]}x{trigger_size[1]}_{exp_num}.sh"
+                    job_script_path = Path(saving_path) / job_script_filename
 
-                        # Read the template and append the command to run the experiment
-                        with open(job_executer_template_path, 'r') as file:
-                            content = file.read()
+                    # Read the template and append the command to run the experiment
+                    with open(job_executer_template_path, 'r') as file:
+                        content = file.read()
 
+                    # Write the new job script
+                    with open(job_script_path, 'w') as file:
+                        file.write(content)
+
+                    for hdlyr_size in hdlyr_size_list:
                         # Command to run the experiment
                         command = f"python training_bd.py --exp_num {exp_num} --saving_path {saving_path} --elm_type {elm_type} --dataset {dataset} --hdlyr_size {hdlyr_size} --trigger_type {trigger_type} --target_label {target_label} --poison_percentage {epsilon} --trigger_size {trigger_size[0]}"
                         
-                        # Write the new job script
-                        with open(job_script_path, 'w') as file:
-                            file.write(content)
+                        # Append the new command in the job script
+                        with open(job_script_path, 'a') as file:
                             file.write("\n")  # Ensure there's a newline before adding the command
                             file.write(command)
 
-                        # Make the script executable
-                        subprocess.run(['chmod', '+x', str(job_script_path)])
-                        # Submit the job script to SLURM
-                        subprocess.run(['sbatch', str(job_script_path)])
+                    # Make the script executable
+                    subprocess.run(['chmod', '+x', str(job_script_path)])
+                    # Submit the job script to SLURM
+                    subprocess.run(['sbatch', str(job_script_path)])
 
 
